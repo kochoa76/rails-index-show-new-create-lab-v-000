cf4de8f3cf9334e4d5362e21a79ff35d5229a237
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-01 17:02:23 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
-
-  create_table "coupons", force: :cascade do |t|
-    t.string   "coupon_code"
-    t.string   "store"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
