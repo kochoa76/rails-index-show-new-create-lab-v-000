1d00122978bffa4c42afd938f2e359a7f3a399f6
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-01 17:19:23 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -12,9 +12,6 @@ class CouponsController < ApplicationController
     @coupon = Coupon.find_by_id(params[:id])
   end 
 
-  end
-
-
   def create
     @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
