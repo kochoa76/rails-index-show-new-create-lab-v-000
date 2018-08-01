b9d2d167bbbf546a09ca56401d74dbf389b25e93
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-01 17:55:24 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -17,7 +17,7 @@ class CouponsController < ApplicationController
     @coupon[:coupon_code]= params[:coupon][:coupon_code]
     @coupon[:store] = params[:coupon][:store]
     @coupon.save
-    redirect to coupon_path(@coupon)
+    redirect_to coupon_path(@coupon)
   end
 
 end
